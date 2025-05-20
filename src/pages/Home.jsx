57704137ff8db7e33c859b8fca82833ae3d22959
Ruchika More <ruchikamore2002@gmail.com>
2025-05-21 01:26:50 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,73 @@
+import MovieCard from "../components/MovieCard";
+import { useState, useEffect} from "react";
+import { searchMovies, getPopularMovies} from "../services/api";
+import "../css/Home.css";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.error(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPopularMovies();
+  }, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) return;
+
+    try {
+      setLoading(true);
+      const results = await searchMovies(searchQuery);
+      setMovies(results);
+      setError(null);
+    } catch (err) {
+      console.error(err);
+      setError("Failed to search movies...");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">Search</button>
+      </form>
+
+      {loading && <p>Loading movies...</p>}
+      {error && <p className="error">{error}</p>}
+      {!loading && !error && movies.length === 0 && (
+        <p>No movies found.</p>
+      )}
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "c6e12c1fab932e29d7bbaee4282cc7dc";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+  );
+  const data = await response.json();
+  return data.results;
+};
